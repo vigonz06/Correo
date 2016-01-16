5dfe6427e2a6ac5f3e12055eac44465834ed892d
vigonz06 <victorglez96@gmail.com>
2016-01-16 13:49:01 +0100

    Escape estaba dos veces

--- fdimail/fdimail/utilsWin.h
+++ fdimail/fdimail/utilsWin.h
@@ -22,7 +22,7 @@ int getKey()
 			key = irInBuf.Event.KeyEvent.wVirtualKeyCode;
 		}
 		else key = irInBuf.Event.KeyEvent.uChar.AsciiChar;
-	} while (key != VK_ESCAPE && key != VK_LEFT && key != VK_UP && key != VK_ESCAPE
+	} while (key != VK_ESCAPE && key != VK_LEFT && key != VK_UP
 		&& key != VK_RIGHT  && key != VK_DOWN && key != VK_RETURN);
 
 	return key;
