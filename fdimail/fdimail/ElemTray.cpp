3fa5ad160bef085e0d23e8adc5858dbc4bb6e56b
vigonz06 <victorglez96@gmail.com>
2017-11-22 10:16:31 +0100

    bool load

--- fdimail/fdimail/ElemTray.cpp
+++ fdimail/fdimail/ElemTray.cpp
@@ -1,18 +1,36 @@
 #include "ElemTray.h"
 #include "Manager.h"
 
-void ElemTray::load(std::ifstream &file)
+bool ElemTray::load(std::ifstream &file)
 {
 	int number;
 	std::string idMail;
 
-	file >> idMail >> read >> number;
-
-	mail = Manager::getManager()->getMailList()->get(idMail);
-	box = bool(number);
-	setId();
+	file >> idMail;
+	
+	if (!file.fail())
+	{
+		file >> read;
+		
+		if (!file.fail())
+		{
+			file >> number;
+			
+			if (!file.fail())
+			{
+				mail = Manager::getManager()->getMailList()->get(idMail);
+				box = bool(number);
+				setId();
+				
+				return true;
+			}
+			else return false;
+		}
+		else return false;
+	}
+	else return false;
 }
-
+	
 void ElemTray::save(std::ofstream &file) const
 {
 	file << mail->getId() << " " << read << " " << box << std::endl;
